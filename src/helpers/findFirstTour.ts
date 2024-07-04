040a733e6ab3fa0088dbcfad003ccf12462e70f9
Sammy Teillet <sammy.teillet@gmail.com>
2024-07-04 20:31:26 +0200

    log results

--- src/helpers/findFirstTour.ts
+++ src/helpers/findFirstTour.ts
@@ -3,18 +3,29 @@ type Resultat = {
   [key: `Nom candidat ${number}`]: string;
   [key: `Prénom candidat ${number}`]: string;
   [key: `Nuance candidat ${number}`]: string;
+  [key: `Voix candidat ${number}`]: string;
 };
 
 export const findEluTour1 = (circo: Resultat) => {
   for (let i = 1; i <= 19; i++) {
-    // @ts-ignore
     if (circo[`Elu ${i}`]) {
       return {
-        // @ts-ignore
         name: `${circo[`Nom candidat ${i}`]} ${circo[`Prénom candidat ${i}`]}`,
-        // @ts-ignore
         nuance: circo[`Nuance candidat ${i}`],
       };
     }
   }
+};
+
+export const findFirstSecondThird = (circo: Resultat) => {
+  const candidates = [];
+  for (let i = 1; i <= 19; i++) {
+    candidates.push({
+      voix: parseInt(circo[`Voix candidat ${i}`]),
+      name: `${circo[`Nom candidat ${i}`]} ${circo[`Prénom candidat ${i}`]}`,
+      nuance: circo[`Nuance candidat ${i}`],
+    });
+  }
+  candidates.sort((a, b) => b.voix - a.voix);
+  return candidates;
 };
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
 import electionjson from "../legislativetour1.json";
-import { findEluTour1 } from "./helpers/findFirstTour";
+import { findEluTour1, findFirstSecondThird } from "./helpers/findFirstTour";
 
 electionjson.forEach((data) => {
   const elu = findEluTour1(data);
   if (elu) {
     console.log(
       `${elu.nuance} - ${data["Libellé département"]} - ${data["Libellé circonscription législative"]} - ${elu.name}`
+    );
+  } else {
+    const candidates = findFirstSecondThird(data);
+    console.log(
+      `${data["Libellé département"]} - ${data["Libellé circonscription législative"]} - ${candidates[0].name} - ${candidates[0].nuance} - ${candidates[1].name} - ${candidates[1].nuance} - ${candidates[2].name} - ${candidates[2].nuance}`
     );
   }
 });
