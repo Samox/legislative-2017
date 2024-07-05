eaba0b9d454e0c00d03d7c1153d50f0e6ae47254
Sammy Teillet <sammy.teillet@gmail.com>
2024-07-05 17:17:03 +0200

    feature: compute result with barage republicain

--- src/helpers/findFirstTour.ts
+++ src/helpers/findFirstTour.ts
@@ -3,7 +3,8 @@ type Resultat = {
   [key: `Nom candidat ${number}`]: string;
   [key: `Prénom candidat ${number}`]: string;
   [key: `Nuance candidat ${number}`]: string;
-  [key: `Voix candidat ${number}`]: string;
+  [key: `Voix ${number}`]: string;
+  [key: `% Voix/inscrits ${number}`]: string;
 };
 
 export const findEluTour1 = (circo: Resultat) => {
@@ -12,20 +13,31 @@ export const findEluTour1 = (circo: Resultat) => {
       return {
         name: `${circo[`Nom candidat ${i}`]} ${circo[`Prénom candidat ${i}`]}`,
         nuance: circo[`Nuance candidat ${i}`],
+        result: parseInt(circo[`% Voix/inscrits ${i}`]),
       };
     }
   }
 };
 
 export const findFirstSecondThird = (circo: Resultat) => {
-  const candidates = [];
+  const candidats = [];
   for (let i = 1; i <= 19; i++) {
-    candidates.push({
-      voix: parseInt(circo[`Voix candidat ${i}`]),
+    candidats.push({
+      voix: parseInt(circo[`Voix ${i}`]),
+      result: parseInt(circo[`% Voix/inscrits ${i}`]),
       name: `${circo[`Nom candidat ${i}`]} ${circo[`Prénom candidat ${i}`]}`,
       nuance: circo[`Nuance candidat ${i}`],
     });
   }
-  candidates.sort((a, b) => b.voix - a.voix);
-  return candidates;
+  candidats.sort((a, b) => b.voix - a.voix);
+  let vainqueur = candidats[0];
+  const RNEnTete = vainqueur.nuance === "RN";
+  if (RNEnTete) {
+    const second = candidats[1];
+    const troisieme = candidats[2];
+    if (troisieme && troisieme.voix + second.voix > vainqueur.voix) {
+      vainqueur = second;
+    }
+  }
+  return { candidates: candidats, vainqueur };
 };
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,23 @@
 import electionjson from "../legislativetour1.json";
 import { findEluTour1, findFirstSecondThird } from "./helpers/findFirstTour";
 
+const resultats: Record<string, number> = {};
+
 electionjson.forEach((data) => {
   const elu = findEluTour1(data);
   if (elu) {
     console.log(
-      `${elu.nuance} - ${data["Libellé département"]} - ${data["Libellé circonscription législative"]} - ${elu.name}`
+      `${elu.nuance} - ${data["Libellé département"]} - ${data["Libellé circonscription législative"]} - ${elu.name} - ${elu.result}`
     );
+    resultats[elu.nuance] = (resultats[elu.nuance] || 0) + 1;
   } else {
-    const candidates = findFirstSecondThird(data);
+    const { candidates, vainqueur } = findFirstSecondThird(data);
+    resultats[vainqueur.nuance] = (resultats[vainqueur.nuance] || 0) + 1;
     console.log(
-      `${data["Libellé département"]} - ${data["Libellé circonscription législative"]} - ${candidates[0].name} - ${candidates[0].nuance} - ${candidates[1].name} - ${candidates[1].nuance} - ${candidates[2].name} - ${candidates[2].nuance}`
+      `${data["Libellé département"]} - ${data["Libellé circonscription législative"]} - Vainqueur ${vainqueur.nuance} - ${vainqueur.name} - ${candidates[0].name} - ${candidates[0].nuance} - ${candidates[0].voix} - ${candidates[1].name} - ${candidates[1].nuance} - ${candidates[1].voix} - ${candidates[2].name} - ${candidates[2].nuance} - ${candidates[2].voix} );
+  }.voix}`
     );
   }
 });
+
+console.log(resultats);
